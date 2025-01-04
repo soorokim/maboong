8ec9800c1676be3bf989e076524628b081a7f25d
soorokim <rlatnfh99@gmail.com>
2025-01-04 18:55:40 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const fs = require("fs-extra");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: "/maboong",
@@ -5,12 +8,19 @@ const nextConfig = {
     config.plugins.push({
       apply: (compiler) => {
         compiler.hooks.done.tap("CopyStaticAssets", () => {
-          const source = path.join(__dirname, ".next/static");
+          const source = path.join(__dirname, ".next/static"); // 빌드된 정적 파일 위치
           const destination = path.join(
             __dirname,
             "public/maboong/_static/asset"
-          );
-          fs.copySync(source, destination);
+          ); // 복사될 대상 경로
+
+          // 디렉터리 복사
+          try {
+            fs.copySync(source, destination);
+            console.log(`Assets copied from ${source} to ${destination}`);
+          } catch (err) {
+            console.error("Error copying assets:", err);
+          }
         });
       },
     });
