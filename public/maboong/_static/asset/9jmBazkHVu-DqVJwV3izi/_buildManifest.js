d3c67f3e55cf6155a1c5c5c3e66d9aaa29c02386
soorokim <rlatnfh99@gmail.com>
2025-01-04 19:02:48 +0900

    test

--- public/maboong/_static/asset/9jmBazkHVu-DqVJwV3izi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-084315d3b19282fb.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
