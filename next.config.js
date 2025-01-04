5f6257912bb14f78981065713a4f5ce60941fbdc
soorokim <rlatnfh99@gmail.com>
2025-01-04 19:20:46 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: "/maboong",
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/maboong/_next/:path+",
+          destination: "/_next/:path+",
+        },
+      ],
+    };
+  },
 };
 
 module.exports = nextConfig;
