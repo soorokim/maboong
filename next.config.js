c63a1768c732e00755020e692f0f9cfa3110da88
soorokim <rlatnfh99@gmail.com>
2025-01-04 18:52:30 +0900

    tset

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: "/maboong",
+  webpack: (config) => {
+    config.plugins.push({
+      apply: (compiler) => {
+        compiler.hooks.done.tap("CopyStaticAssets", () => {
+          const source = path.join(__dirname, ".next/static");
+          const destination = path.join(
+            __dirname,
+            "public/maboong/_static/asset"
+          );
+          fs.copySync(source, destination);
+        });
+      },
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
